9d83cbcdd62ad15350e2be4cdd45b91fdb73816d
kmille <christian.schneider@androidloves.me>
2017-09-27 13:26:57 +0200

    Disabled debug

--- main.py
+++ main.py
@@ -486,5 +486,5 @@ def handle_generic_error(err):
 
 # run app
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', debug=False, threaded=True)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False, threaded=True)
+    #app.run(host='0.0.0.0', debug=True)
